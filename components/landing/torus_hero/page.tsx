4602573519aa474c6530e5d09df4b7d50c17dfdc
Damien-Oujami <blurbdaddy@proton.me>
2025-08-25 11:22:01 -0400

    Create page.tsx

---
+++ components/landing/torus_hero/page.tsx
@@ -0,0 +1,10 @@
+import TekitaHero from "@/components/TekitaHero";
+
+export default function Home() {
+  return (
+    <main>
+      <TekitaHero />
+      {/* other sections... */}
+    </main>
+  );
+}
